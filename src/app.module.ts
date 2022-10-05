701f8a9ced89522fa269af982cac52f9d24b45b9
Vladyslav Vintoniuk <spatiumfabri@gmail.com>
2022-10-05 15:44:36 +0300

    Setup migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from "./auth/auth.module";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { UserModel } from "./auth/user.model";
+import { UserModel } from "./user/user.model";
 
 import * as dotenv from 'dotenv';
+import {UserModule} from "./user/user.module";
 
 dotenv.config()
 
@@ -23,6 +24,7 @@ dotenv.config()
                     {ssl: { require: true,
                             rejectUnauthorized: false }}
             }),
+      UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {UserDbService} from "./user.db.service";
+import {UserDbService} from "../user/user.db.service";
 import {SequelizeModule} from "@nestjs/sequelize";
-import {UserModel} from './user.model';
+import {UserModel} from '../user/user.model';
 import { JwtModule } from "@nestjs/jwt";
+
 
 @Module({
     imports: [SequelizeModule.forFeature([UserModel]),
